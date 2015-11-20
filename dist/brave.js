4105844b88d18143d7c7bf44e01bf1bf438e6dea
David Stone <davidjamesstone@gmail.com>
2015-11-20 00:31:46 +0000

    Added DOM event attributes

--- dist/brave.js
+++ dist/brave.js
@@ -1,6 +1,10 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Brave = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var get = require('get-object-path')
 var Delegate = require('dom-delegate').Delegate
+var onEvents = Object.getOwnPropertyNames(document).concat(Object.getOwnPropertyNames(Object.getPrototypeOf(Object.getPrototypeOf(document)))).concat(Object.getOwnPropertyNames(Object.getPrototypeOf(window))).filter(function (i) {return !i.indexOf('on') && (document[i] == null || typeof document[i] == 'function');}).filter(function (elem, pos, self) {return self.indexOf(elem) == pos;})
+var onEventsSelector = onEvents.map(function (key) {
+  return '[' + key + ']:not([' + key + '=""])'
+}).join(',')
 
 function Register () {}
 Object.defineProperties(Register.prototype, {
@@ -146,95 +150,139 @@ function findParentContext (el, contexts) {
   do {
     el = el.parentNode
     if (el) {
-      for (var i = contexts.length - 1; i > -1 ; i--) {
+      for (var i = contexts.length - 1; i > -1; i--) {
         if (contexts[i].ctx.el === el) {
           return contexts[i].ctx
         }
       }
     }
   } while (el)
-}
-
-function setHtml (el, component, ctx) {
-  var html = (typeof component.template === 'function')
-    ? component.template.call(ctx, ctx)
-    : component.template
-
-  el.innerHTML = html
-}
-
-function renderer (currEl, component, ctx) {
-  return function () {
-    setHtml(currEl, component, ctx)
-    Dom.scan(currEl, ctx.data, ctx, true)
-  }
-}
-
-function scan (el, data, parent, childrenOnly) {
-  var matches = getMatchingElements(el, childrenOnly)
-  var contexts = []
-  if (parent) {
-    contexts.push({ctx: parent})
   }
 
-  var currEl
-  while (matches.length) {
-    currEl = matches.shift()
-    var ref = getElementComponent(currEl)
-    var component = ref.component
-    var parentContext = findParentContext(currEl, contexts) || parent
-    var parentData = parentContext ? parentContext.data : data
-    var elData = getElementData(currEl, ref.key, parentData) || parentData
-    var ctx = createContext(currEl, elData, component, parentContext)
-    var del = createElementDelegate(currEl, ctx, component)
+  function setHtml (el, component, ctx) {
+    var html = (typeof component.template === 'function')
+      ? component.template.call(ctx, ctx)
+      : component.template
 
-    Object.defineProperty(ctx.__, 'del', { value: del })
-
-    extend(ctx, component)
-
-    contexts.push({
-      key: ref.key, ctx: ctx, initialize: component.initialize,
-      template: component.template, component: component, el: currEl
-    })
+    el.innerHTML = html
   }
 
-  var i, j
-  var processed = []
-  for (i = contexts.length - 1; i >= 0; i--) {
-    var aliasContext = contexts[i].ctx
-    var aliasEl = aliasContext.el
-    var aliases = aliasEl.querySelectorAll('[as]:not([as=""])')
-    for (j = 0; j < aliases.length; j++) {
-      if (processed.indexOf(aliases[j]) < 0) {
-        var attr = aliases[j].getAttribute('as')
-        aliasContext[attr] = aliases[j]
-        processed.push(aliases[j])
+  function renderer (currEl, component, ctx) {
+    return function () {
+      setHtml(currEl, component, ctx)
+      Dom.scan(currEl, ctx.data, ctx, true)
+    }
+  }
+
+  function scan (el, data, parent, childrenOnly) {
+    var matches = getMatchingElements(el, childrenOnly)
+    var contexts = []
+    if (parent) {
+      contexts.push({ctx: parent})
+    }
+
+    var currEl
+    while (matches.length) {
+      currEl = matches.shift()
+      var ref = getElementComponent(currEl)
+      var component = ref.component
+      var parentContext = findParentContext(currEl, contexts) || parent
+      var parentData = parentContext ? parentContext.data : data
+      var elData = getElementData(currEl, ref.key, parentData) || parentData
+      var ctx = createContext(currEl, elData, component, parentContext)
+      var del = createElementDelegate(currEl, ctx, component)
+
+      Object.defineProperty(ctx.__, 'del', { value: del })
+
+      extend(ctx, component)
+
+      contexts.push({
+        key: ref.key, ctx: ctx, initialize: component.initialize,
+        template: component.template, component: component, el: currEl
+      })
+    }
+
+    var i, j
+    var processed = []
+    for (i = contexts.length - 1; i >= 0; i--) {
+      var aliasContext = contexts[i].ctx
+      var aliasEl = aliasContext.el
+      var aliases = aliasEl.querySelectorAll('[as]:not([as=""])')
+      for (j = 0; j < aliases.length; j++) {
+        if (processed.indexOf(aliases[j]) < 0) {
+          var attr = aliases[j].getAttribute('as')
+          aliasContext[attr] = aliases[j]
+          processed.push(aliases[j])
+        }
+      }
+    }
+
+    // processed = []
+    // for (i = contexts.length - 1; i >= 0; i--) {
+    //   var onContext = contexts[i].ctx
+    //   var onEl = onContext.el
+    //   var ons = onEl.querySelectorAll('[onclick]:not([onclick=""])')
+    //   for (j = 0; j < ons.length; j++) {
+    //     if (processed.indexOf(ons[j]) < 0) {
+    //       attr = ons[j].getAttribute('onclick')
+    //       // var fn = ons[j].onclick
+    //       var fn = new Function('with (this) {\n\treturn ' + attr + '\n}')
+    //       ons[j].onclick = fn.bind(onContext)
+    //       processed.push(ons[j])
+    //     }
+    //   }
+    // }
+    processed = []
+    for (i = contexts.length - 1; i >= 0; i--) {
+      var onContext = contexts[i].ctx
+      var onEl = onContext.el
+      var ons = nodeListToArray(onEl.querySelectorAll(onEventsSelector))
+      ons.unshift(onEl)
+      for (j = 0; j < ons.length; j++) {
+        if (processed.indexOf(ons[j]) < 0) {
+          processed.push(ons[j])
+          for (var k = 0; k < onEvents.length; k++) {
+            if (ons[j].attributes[onEvents[k]]) {
+              attr = ons[j].attributes[onEvents[k]].value
+              // var fn = ons[j].onclick
+              // var fn = new Function('e', 'with (this) {\n\treturn ' + attr + '\n}')
+              // ons[j][onEvents[k]] = fn.bind(onContext)
+              function handler (fn, ctx) {
+                return function (e) {
+                  return fn.call(this, e, ctx)
+                }
+              }
+
+              var fn = new Function('e, ctx', 'with (ctx) {\n\treturn ' + attr + '\n}')
+              ons[j][onEvents[k]] = handler(fn, onContext)
+            }
+          }
+        }
+      }
+    }
+
+    for (i = 0; i < contexts.length; i++) {
+      if (contexts[i].initialize) {
+        contexts[i].initialize.call(contexts[i].ctx)
+      }
+    }
+
+    for (i = 0; i < contexts.length; i++) {
+      if (contexts[i].template) {
+        var render = renderer(contexts[i].ctx.el, contexts[i].component, contexts[i].ctx)
+        render()
+        contexts[i].ctx.render = render
       }
     }
   }
 
-  for (i = 0; i < contexts.length; i++) {
-    if (contexts[i].initialize) {
-      contexts[i].initialize.call(contexts[i].ctx)
-    }
-  }
+  var Dom = Object.create({}, {
+    _register: { value: register },
+    register: { value: registerComponent },
+    scan: { value: scan }
+  })
 
-  for (i = 0; i < contexts.length; i++) {
-    if (contexts[i].template) {
-      var render = renderer(contexts[i].ctx.el, contexts[i].component, contexts[i].ctx)
-      render()
-      contexts[i].ctx.render = render
-    }
-  }
-}
-
-var Dom = Object.create({}, {
-  _register: { value: register },
-  register: { value: registerComponent },
-  scan: { value: scan }
-})
-
-module.exports = Dom
+  module.exports = Dom
 
 },{"dom-delegate":3,"get-object-path":4}],2:[function(require,module,exports){
 /*jshint browser:true, node:true*/
